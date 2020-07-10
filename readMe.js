57671dec156e108756ab226f7837085907973b0e
Isaias Del Mundo <idelmundo68@gmail.com>
2020-07-09 21:46:45 -0700

    gitignore

--- readMe.js
+++ readMe.js
@@ -7,14 +7,19 @@ const questions = [{
         name: "Name"
     },
     {
+        type: "input"
+    } {
         type: "list",
         message: "",
         name: "badge",
-        choices: []
+        choices: ["GPL", "ISC", "UCB"
+            "MIT"
+        ]
     },
 ];
 getUserInput(() => {
             inquirer.prompt(questions)
                 .then(functon(response) {
                         console.log(response);
-                        var text =
+                        var text = #Title: $ { response.Title }###
+                        Github[![License: CC BY - SA 4.0](https: //img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)

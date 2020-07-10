427fee955859c2543c0c9c444dfeeec696bc1ece
Isaias Del Mundo <idelmundo68@gmail.com>
2020-07-09 23:54:16 -0700

    updatejs

--- readMe.js
+++ readMe.js
@@ -1,25 +1,66 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const questions = [{
-        type: "input",
-        message: "create github name",
-        name: "Name"
-    },
-    {
-        type: "input"
-    } {
-        type: "list",
-        message: "",
-        name: "badge",
-        choices: ["GPL", "ISC", "UCB"
-            "MIT"
-        ]
-    },
-];
-getUserInput(() => {
-            inquirer.prompt(questions)
-                .then(functon(response) {
-                        console.log(response);
-                        var text = #Title: $ { response.Title }###
-                        Github[![License: CC BY - SA 4.0](https: //img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)
+inquirer
+    .prompt([{
+            type: "input",
+            message: "Create project title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "what is the description?",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "What did you install?",
+            name: "install"
+        }, {
+            type: "input",
+            message: "Usage?",
+            name: "Usage"
+        }, {
+            type: "input",
+            message: "What type of license?",
+            name: "license"
+        }, {
+            type: "input",
+            message: "who contribute?",
+            name: "contributing"
+        }, {
+            type: "input",
+            message: "what tests?",
+            name: "test"
+        }, {
+            type: "input",
+            message: "Questions",
+            name: "Questions"
+        },
+    ]).then(function(data) {
+        console.log(data);
+        var generate = `
+  ## Project title: ${Response.title}
+  
+  ## Description: ${Response.description}
+  
+  ### Table of Contents: 
+
+  ## Installation: ${Response.install}
+
+  ## Usage: ${Response.Usage}
+
+  ## License: ${Response.license}
+
+  ## Contributing: ${Response.Contributing}
+
+  ## Tests: ${Response.test}
+ 
+  ## Questions: $(Response.Questions)
+  * User Github Profile email
+  * User Github email
+  `;
+    })
+fs.writeFile("readMe.md", generate, data, function(err) {
+            console.log("do i work)
+            })

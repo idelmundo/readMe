8df42f4d030ec90ce3e40090a9d15e144791ac0e
Isaias Del Mundo <idelmundo68@gmail.com>
2020-07-11 01:06:42 -0700

    testing axios

--- readMe.js
+++ readMe.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-
 const axios = require("axios")
 
 let questions = [{
@@ -70,5 +69,28 @@ function userInputs() {
         })
 }
 
+inquirer
+    .prompt({
+        message: "Enter your GitHub username:",
+        name: "username"
+    })
+    .then(function({ username }) {
+        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
+        axios.get(queryUrl).then(function(res) {
+            const repoNames = res.data.map(function(repo) {
+                return repo.name;
+            });
+
+            const repoNamesStr = repoNames.join("\n");
+
+            fs.writeFile("repos.txt", repoNamesStr, function(err) {
+                if (err) {
+                    throw err;
+                }
+
+                console.log(`Saved ${repoNames.length} repos`);
+            });
+        });
+    });
 userInputs();
